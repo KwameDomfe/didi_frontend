9c2852b4d15db60bd6727441b20371d0f28b9ee4
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-04-23 09:29:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ import MainHeader from './components/MainHeader.jsx';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://didi-app-feixt.ondigitalocean.app";
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api";
     const [restaurants, setRestaurants] = useState([]);
     const [menuItems, setMenuItems] = useState([]);
     const [loading, setLoading] = useState(false);
